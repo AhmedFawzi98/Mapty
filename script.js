599fb263fefdf4e551fcfb1f80ba5a19cdced16d
Ahmed Fawzi <ahmedfawzi636@gmail.com>
2026-03-25 22:01:45 +0200

    Implement add new workout functionality

--- script.js
+++ script.js
@@ -25,6 +25,7 @@ class Workout {
 }
 
 class RunningWorkout extends Workout {
+    type = "running";
     constructor(distance, coords, duration, cadence) {
         super(distance, coords, duration);
         this.cadence = cadence;
@@ -36,6 +37,7 @@ class RunningWorkout extends Workout {
 }
 
 class CyclingWorkout extends Workout {
+    type = "cycling";
     constructor(distance, coords, duration, elevationGain) {
         super(distance, coords, duration);
         this.elevationGain = elevationGain;
@@ -82,7 +84,7 @@ class App {
         this.#map = L.map("map").setView(coords, this.#mapZoom);
         this.#map.on("click", (mapEv) => {
             this.#mapEvent = mapEv;
-            this.#showform();
+            this.#Openform();
         });
 
         L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
@@ -99,33 +101,94 @@ class App {
         inputType.addEventListener("change", this.#toggleFormFields);
     }
 
-    #showform() {
-        form.classList.remove("hidden");
-        inputDistance.focus();
-    }
-
     #addNewWorkout() {
-        this.#createMarkerWithPopup();
-        form.reset();
+        const type = inputType.value;
+        const distance = Number(inputDistance.value);
+        const duration = Number(inputDuration.value);
+        const { lat, lng } = this.#mapEvent.latlng;
+        const coords = [lat, lng];
+        let workout;
+        if (type === "running") {
+            const cadence = Number(inputCadence.value);
+            const isValid = this.#isValidInputData(
+                type,
+                distance,
+                duration,
+                cadence,
+            );
+            if (!isValid) {
+                return alert("invalid input.");
+            }
+
+            workout = new RunningWorkout(distance, coords, duration, cadence);
+        } else if (type === "cycling") {
+            const elevationGain = Number(inputElevation.value);
+            const isValid = this.#isValidInputData(
+                type,
+                distance,
+                duration,
+                elevationGain,
+            );
+
+            if (!isValid) {
+                return alert("invalid input.");
+            }
+
+            workout = new CyclingWorkout(
+                distance,
+                coords,
+                duration,
+                elevationGain,
+            );
+        }
+
+        this.#workouts.push(workout);
+
+        this.#createMarkerWithPopup(workout, type);
+
+        //render workout in the list
+
+        this.#closeForm();
     }
 
-    #createMarkerWithPopup() {
-        const { lat, lng } = this.#mapEvent.latlng;
-        const markerCoords = [lat, lng];
+    #isValidInputData(type, ...numericInputs) {
+        if (type !== "running" && type !== "cycling") return false;
+        return numericInputs.every(
+            (numericInput) => Number.isFinite(numericInput) && numericInput > 0,
+        );
+    }
+
+    #createMarkerWithPopup(workout) {
+        const formattedWorkoutDate = new Intl.DateTimeFormat("en-US", {
+            day: "numeric",
+            month: "long",
+            year: "numeric",
+        }).format(workout.date);
+
         const popUpOptions = {
-            maxWidth: 250,
+            maxWidth: 300,
             minWidth: 100,
             autoClose: false,
             closeOnClick: false,
-            className: "running-popup",
+            className: `${workout.type}-popup`,
         };
         const popup = L.popup(popUpOptions);
-        const popupContent = "workout";
-        const marker = L.marker(markerCoords)
+        const popupContent = `${workout.type} ${workout.distance} km for ${workout.duration} min - on ${formattedWorkoutDate}`;
+        const marker = L.marker(workout.coords)
             .addTo(this.#map)
             .bindPopup(popup)
             .setPopupContent(popupContent)
             .openPopup();
+    }
+
+    #Openform() {
+        form.classList.remove("hidden");
+        inputDistance.focus();
+    }
+
+    #closeForm() {
+        form.reset();
+        form.classList.add("hidden");
     }
 
     #toggleFormFields() {
